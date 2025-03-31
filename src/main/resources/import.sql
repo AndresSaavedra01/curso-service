1c77cf35355ce7c48bf8c6f639768d96f9ed4dff
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-03-31 17:03:27 -0500

    feat: Datos de prueba en import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,74 @@
+INSERT INTO curso (activo, cupos_disponibles, descripcion, duracion, fecha_creacion, horario, id_docente, id_semestre, modalidad, nombre, numero_creditos)
+VALUES (
+           1,
+           30,
+           'Curso introductorio de matemáticas para principiantes',
+           60,
+           '2023-01-10',
+           'Lunes y Jueves 09:00 - 11:00',
+           101,
+           202301,
+           'Presencial',
+           'Matemáticas Básicas',
+           4
+       );
+
+INSERT INTO curso (activo, cupos_disponibles, descripcion, duracion, fecha_creacion, horario, id_docente, id_semestre, modalidad, nombre, numero_creditos)
+VALUES (
+           1,
+           25,
+           'Fundamentos de programación en Java y algoritmos',
+           90,
+           '2023-02-15',
+           'Martes y Viernes 14:00 - 16:00',
+           102,
+           202302,
+           'Virtual',
+           'Programación I',
+           5
+       );
+
+INSERT INTO curso (activo, cupos_disponibles, descripcion, duracion, fecha_creacion, horario, id_docente, id_semestre, modalidad, nombre, numero_creditos)
+VALUES (
+           0,
+           0,
+           'Curso Introductorio',
+           45,
+           '2022-09-01',
+           'Miércoles 10:00 - 13:00',
+           103,
+           202202,
+           'Híbrido',
+           'Analisis Numerico',
+           3
+       );
+
+INSERT INTO curso (activo, cupos_disponibles, descripcion, duracion, fecha_creacion, horario, id_docente, id_semestre, modalidad, nombre, numero_creditos)
+VALUES (
+           1,
+           20,
+           'Curso avanzado de inglés con enfoque en conversación',
+           80,
+           '2023-03-20',
+           'Lunes a Viernes 08:00 - 10:00',
+           104,
+           202303,
+           'Presencial',
+           'Inglés Avanzado',
+           4
+       );
+
+INSERT INTO curso (activo, cupos_disponibles, descripcion, duracion, fecha_creacion, horario, id_docente, id_semestre, modalidad, nombre, numero_creditos)
+VALUES (
+           1,
+           15,
+           'Introducción a los conceptos básicos de html',
+           50,
+           '2023-04-05',
+           'Sábados 09:00 - 12:00',
+           105,
+           202303,
+           'Virtual',
+           'Ambiente Web I',
+           3
+       );
