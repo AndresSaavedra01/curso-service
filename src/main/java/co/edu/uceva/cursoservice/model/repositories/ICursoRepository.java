5f1c1694ef55c5650acc3b706e91cc6bb478c46b
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-03-31 15:54:06 -0500

    feat: Creacion del repositorio para la entidad curso

---
+++ src/main/java/co/edu/uceva/cursoservice/model/repositories/ICursoRepository.java
@@ -0,0 +1,7 @@
+package co.edu.uceva.cursoservice.model.repositories;
+
+import co.edu.uceva.cursoservice.model.entities.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICursoRepository extends CrudRepository<Curso, Long> {
+}
